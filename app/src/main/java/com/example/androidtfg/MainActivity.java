2b6352d7391f32efe3306c01174f88f39ad58fc5
ar98nau <anr3@alumnes.udl.cat>
2020-08-13 17:37:28 +0200

    Added Firebase and permanent Files
    Signed-off-by: ar98nau <anr3@alumnes.udl.cat>

--- app/src/main/java/com/example/androidtfg/MainActivity.java
+++ app/src/main/java/com/example/androidtfg/MainActivity.java
@@ -3,15 +3,21 @@ package com.example.androidtfg;
 import android.Manifest;
 import android.app.Activity;
 import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
-import android.os.Environment;
-import android.support.v4.app.ActivityCompat;
-import android.support.v7.app.AppCompatActivity;
+
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.util.Log;
 import android.view.SurfaceView;
 import android.view.View;
 import android.widget.Toast;
 
 import com.example.androidseries.R;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 
 import org.opencv.android.BaseLoaderCallback;
 import org.opencv.android.CameraBridgeViewBase;
@@ -31,8 +37,13 @@ import org.opencv.dnn.Net;
 import org.opencv.imgproc.Imgproc;
 import org.opencv.utils.Converters;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 
@@ -43,11 +54,17 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity implements CameraBridgeViewBase.CvCameraViewListener2 {
 
+    private static final String TAG = "MainActivity";
+    private static final int MY_CAMERA_REQUEST_CODE = 100;
     CameraBridgeViewBase cameraBridgeViewBase;
     BaseLoaderCallback baseLoaderCallback;
     boolean startYolo = false;
-    boolean firstTimeYolo = false;
     Net tinyYolo;
+    String tinyYoloCfg;
+    String tinyYoloWeights;
+    private StorageReference mStorageRef;
+    private ArrayList<String> cocoNames;
+    private boolean netInitialized = false;
 
     private static final int REQUEST_EXTERNAL_STORAGE = 1;
     private static String[] PERMISSIONS_STORAGE = {
@@ -56,289 +73,53 @@ public class MainActivity extends AppCompatActivity implements CameraBridgeViewB
     };
 
 
-
-    public void YOLO(View Button){
-
-        if (startYolo == false){
-
-
-
-
-            startYolo = true;
-
-            if (firstTimeYolo == false){
-
-
-                firstTimeYolo = true;
-                String tinyYoloCfg = Environment.getExternalStorageDirectory() + "/dnns/yolov3-tiny.cfg" ;
-                String tinyYoloWeights = Environment.getExternalStorageDirectory() + "/dnns/yolov3-tiny.weights";
-
-                tinyYolo = Dnn.readNetFromDarknet(tinyYoloCfg, tinyYoloWeights);
-
-
-
-
-
-
-
-
-
-            }
-
-
-
-        }
-
-        else{
-
-            startYolo = false;
-
-
-        }
-
-
-
-
-    }
-
-
-
-
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
         cameraBridgeViewBase = (JavaCameraView)findViewById(R.id.CameraView);
         cameraBridgeViewBase.setVisibility(SurfaceView.VISIBLE);
         cameraBridgeViewBase.setCvCameraViewListener(this);
 
+        mStorageRef = FirebaseStorage.getInstance().getReference();
+
         verifyStoragePermissions(this);
 
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (checkSelfPermission(Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+                requestPermissions(new String[]{Manifest.permission.CAMERA}, MY_CAMERA_REQUEST_CODE);
+            }
+        }
 
-        //System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
         baseLoaderCallback = new BaseLoaderCallback(this) {
             @Override
             public void onManagerConnected(int status) {
                 super.onManagerConnected(status);
 
-                switch(status){
-
-                    case BaseLoaderCallback.SUCCESS:
-                        cameraBridgeViewBase.enableView();
-                        break;
-                    default:
-                        super.onManagerConnected(status);
-                        break;
+                if (status == BaseLoaderCallback.SUCCESS) {
+                    cameraBridgeViewBase.enableView();
+                } else {
+                    super.onManagerConnected(status);
                 }
-
 
             }
 
         };
 
-
-
-
     }
 
     @Override
-    public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
+    protected void onStart() {
+        Log.d(TAG, "onStart");
 
-        Mat frame = inputFrame.rgba();
-
-        if (startYolo == true) {
-
-            Imgproc.cvtColor(frame, frame, Imgproc.COLOR_RGBA2RGB);
-
-
-
-            Mat imageBlob = Dnn.blobFromImage(frame, 0.00392, new Size(416,416),new Scalar(0, 0, 0),/*swapRB*/false, /*crop*/false);
-
-
-            tinyYolo.setInput(imageBlob);
-
-
-
-            List<Mat> result = new ArrayList<Mat>(2);
-
-            List<String> outBlobNames = new ArrayList<>();
-            outBlobNames.add(0, "yolo_16");
-            outBlobNames.add(1, "yolo_23");
-
-            tinyYolo.forward(result,outBlobNames);
-
-
-            float confThreshold = 0.3f;
-
-
-
-            List<Integer> clsIds = new ArrayList<>();
-            List<Float> confs = new ArrayList<>();
-            List<Rect> rects = new ArrayList<>();
-
-
-
-
-            for (int i = 0; i < result.size(); ++i)
-            {
-
-                Mat level = result.get(i);
-
-                for (int j = 0; j < level.rows(); ++j)
-                {
-                    Mat row = level.row(j);
-                    Mat scores = row.colRange(5, level.cols());
-
-                    Core.MinMaxLocResult mm = Core.minMaxLoc(scores);
-
-
-
-
-                    float confidence = (float)mm.maxVal;
-
-
-                    Point classIdPoint = mm.maxLoc;
-
-
-
-                    if (confidence > confThreshold)
-                    {
-                        int centerX = (int)(row.get(0,0)[0] * frame.cols());
-                        int centerY = (int)(row.get(0,1)[0] * frame.rows());
-                        int width   = (int)(row.get(0,2)[0] * frame.cols());
-                        int height  = (int)(row.get(0,3)[0] * frame.rows());
-
-
-                        int left    = centerX - width  / 2;
-                        int top     = centerY - height / 2;
-
-                        clsIds.add((int)classIdPoint.x);
-                        confs.add((float)confidence);
-
-
-
-
-                        rects.add(new Rect(left, top, width, height));
-                    }
-                }
-            }
-            int ArrayLength = confs.size();
-
-            if (ArrayLength>=1) {
-                // Apply non-maximum suppression procedure.
-                float nmsThresh = 0.2f;
-
-
-
-
-                MatOfFloat confidences = new MatOfFloat(Converters.vector_float_to_Mat(confs));
-
-
-                Rect[] boxesArray = rects.toArray(new Rect[0]);
-
-                MatOfRect boxes = new MatOfRect(boxesArray);
-
-                MatOfInt indices = new MatOfInt();
-
-
-
-                Dnn.NMSBoxes(boxes, confidences, confThreshold, nmsThresh, indices);
-
-
-                // Draw result boxes:
-                int[] ind = indices.toArray();
-                for (int i = 0; i < ind.length; ++i) {
-
-                    int idx = ind[i];
-                    Rect box = boxesArray[idx];
-
-                    int idGuy = clsIds.get(idx);
-
-                    float conf = confs.get(idx);
-
-
-                    List<String> cocoNames = Arrays.asList("a person", "a bicycle", "a motorbike", "an airplane", "a bus", "a train", "a truck", "a boat", "a traffic light", "a fire hydrant", "a stop sign", "a parking meter", "a car", "a bench", "a bird", "a cat", "a dog", "a horse", "a sheep", "a cow", "an elephant", "a bear", "a zebra", "a giraffe", "a backpack", "an umbrella", "a handbag", "a tie", "a suitcase", "a frisbee", "skis", "a snowboard", "a sports ball", "a kite", "a baseball bat", "a baseball glove", "a skateboard", "a surfboard", "a tennis racket", "a bottle", "a wine glass", "a cup", "a fork", "a knife", "a spoon", "a bowl", "a banana", "an apple", "a sandwich", "an orange", "broccoli", "a carrot", "a hot dog", "a pizza", "a doughnut", "a cake", "a chair", "a sofa", "a potted plant", "a bed", "a dining table", "a toilet", "a TV monitor", "a laptop", "a computer mouse", "a remote control", "a keyboard", "a cell phone", "a microwave", "an oven", "a toaster", "a sink", "a refrigerator", "a book", "a clock", "a vase", "a pair of scissors", "a teddy bear", "a hair drier", "a toothbrush");
-
-
-
-                    int intConf = (int) (conf * 100);
-
-
-
-                    Imgproc.putText(frame,cocoNames.get(idGuy) + " " + intConf + "%",box.tl(),Core.FONT_HERSHEY_SIMPLEX, 2, new Scalar(255,255,0),2);
-
-
-
-                    Imgproc.rectangle(frame, box.tl(), box.br(), new Scalar(255, 0, 0), 2);
-
-
-
-
-
-                }
-            }
-
-
-
-
-
-
-
-
-
-        }
-
-
-
-        return frame;
+        super.onStart();
     }
-
-    public static void verifyStoragePermissions(Activity activity) {
-        // Check if we have write permission
-        int permission = ActivityCompat.checkSelfPermission(activity, Manifest.permission.WRITE_EXTERNAL_STORAGE);
-
-        if (permission != PackageManager.PERMISSION_GRANTED) {
-            // We don't have permission so prompt the user
-            ActivityCompat.requestPermissions(
-                    activity,
-                    PERMISSIONS_STORAGE,
-                    REQUEST_EXTERNAL_STORAGE
-            );
-        }
-    }
-
-
-    @Override
-    public void onCameraViewStarted(int width, int height) {
-
-
-        if (startYolo == true){
-
-            String tinyYoloCfg = Environment.getExternalStorageDirectory() + "/dnns/yolov3-tiny.cfg" ;
-            String tinyYoloWeights = Environment.getExternalStorageDirectory() + "/dnns/yolov3-tiny.weights";
-
-            tinyYolo = Dnn.readNetFromDarknet(tinyYoloCfg, tinyYoloWeights);
-
-
-        }
-
-
-
-    }
-
-
-    @Override
-    public void onCameraViewStopped() {
-
-    }
-
 
     @Override
     protected void onResume() {
+        Log.d(TAG, "onResume");
+
         super.onResume();
 
         if (!OpenCVLoader.initDebug()){
@@ -356,6 +137,8 @@ public class MainActivity extends AppCompatActivity implements CameraBridgeViewB
 
     @Override
     protected void onPause() {
+        Log.d(TAG, "onPause");
+
         super.onPause();
         if(cameraBridgeViewBase!=null){
 
@@ -364,12 +147,280 @@ public class MainActivity extends AppCompatActivity implements CameraBridgeViewB
 
     }
 
+    @Override
+    protected void onStop() {
+        Log.d(TAG, "onStop");
+
+        super.onStop();
+    }
 
     @Override
     protected void onDestroy() {
+        Log.d(TAG, "onDestroy");
+
         super.onDestroy();
         if (cameraBridgeViewBase!=null){
             cameraBridgeViewBase.disableView();
         }
+
     }
+
+    @Override
+    public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
+
+        Mat frame = inputFrame.rgba();
+
+        if (startYolo && netInitialized) {
+
+            Imgproc.cvtColor(frame, frame, Imgproc.COLOR_RGBA2RGB);
+
+            Mat imageBlob = Dnn.blobFromImage(frame, 0.00392, new Size(416,416),new Scalar(0, 0, 0),/*swapRB*/false, /*crop*/false);
+
+            tinyYolo.setInput(imageBlob);
+
+            List<Mat> result = new ArrayList<Mat>(2);
+
+            List<String> outBlobNames = getOutputNames(tinyYolo);
+
+            tinyYolo.forward(result,outBlobNames);
+
+            float confThreshold = 0.3f;
+
+            List<Integer> clsIds = new ArrayList<>();
+            List<Float> confs = new ArrayList<>();
+            List<Rect> rects = new ArrayList<>();
+
+            for (int i = 0; i < result.size(); ++i)
+            {
+
+                Mat level = result.get(i);
+
+                for (int j = 0; j < level.rows(); ++j)
+                {
+                    Mat row = level.row(j);
+                    Mat scores = row.colRange(5, level.cols());
+
+                    Core.MinMaxLocResult mm = Core.minMaxLoc(scores);
+
+                    float confidence = (float)mm.maxVal;
+
+                    Point classIdPoint = mm.maxLoc;
+
+                    if (confidence > confThreshold)
+                    {
+                        int centerX = (int)(row.get(0,0)[0] * frame.cols());
+                        int centerY = (int)(row.get(0,1)[0] * frame.rows());
+                        int width   = (int)(row.get(0,2)[0] * frame.cols());
+                        int height  = (int)(row.get(0,3)[0] * frame.rows());
+
+                        int left    = centerX - width  / 2;
+                        int top     = centerY - height / 2;
+
+                        clsIds.add((int)classIdPoint.x);
+                        confs.add(confidence);
+
+                        rects.add(new Rect(left, top, width, height));
+                    }
+                }
+            }
+            int ArrayLength = confs.size();
+
+            if (ArrayLength>=1) {
+                // Apply non-maximum suppression procedure.
+                float nmsThresh = 0.2f;
+
+                MatOfFloat confidences = new MatOfFloat(Converters.vector_float_to_Mat(confs));
+
+
+                Rect[] boxesArray = rects.toArray(new Rect[0]);
+
+                MatOfRect boxes = new MatOfRect(boxesArray);
+
+                MatOfInt indices = new MatOfInt();
+
+                Dnn.NMSBoxes(boxes, confidences, confThreshold, nmsThresh, indices);
+
+                // Draw result boxes:
+                int[] ind = indices.toArray();
+                for (int i = 0; i < ind.length; ++i) {
+
+                    int idx = ind[i];
+                    Rect box = boxesArray[idx];
+
+                    int idGuy = clsIds.get(idx);
+
+                    float conf = confs.get(idx);
+
+                    int intConf = (int) (conf * 100);
+
+                    Imgproc.putText(frame,cocoNames.get(idGuy) + " " + intConf + "%",box.tl(),Core.FONT_HERSHEY_SIMPLEX, 2, new Scalar(255,255,0),2);
+
+                    Imgproc.rectangle(frame, box.tl(), box.br(), new Scalar(255, 0, 0), 2);
+
+                }
+            }
+        }
+
+
+
+        return frame;
+    }
+
+    @Override
+    public void onCameraViewStarted(int width, int height) {
+        Log.d(TAG, "onCameraViewStarted");
+
+    }
+
+    @Override
+    public void onCameraViewStopped() {
+        Log.d(TAG, "onCameraViewStopped");
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == MY_CAMERA_REQUEST_CODE) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(this, "camera permission granted", Toast.LENGTH_LONG).show();
+            } else {
+                Toast.makeText(this, "camera permission denied", Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+
+    public static void verifyStoragePermissions(Activity activity) {
+        // Check if we have write permission
+        int permission = ActivityCompat.checkSelfPermission(activity, Manifest.permission.WRITE_EXTERNAL_STORAGE);
+
+        if (permission != PackageManager.PERMISSION_GRANTED) {
+            // We don't have permission so prompt the user
+            ActivityCompat.requestPermissions(
+                    activity,
+                    PERMISSIONS_STORAGE,
+                    REQUEST_EXTERNAL_STORAGE
+            );
+        }
+    }
+
+    public void YOLO(View Button){
+
+        if (!startYolo){
+
+            startYolo = true;
+
+            if (!netInitialized){
+                downloadModelsFromFirebase();
+                Log.d(TAG, "STARTED FROM BUTTON - FIRST TIME");
+
+            }
+
+            Log.d(TAG, "STARTED FROM BUTTON");
+
+        } else {
+
+            startYolo = false;
+            Log.d(TAG, "STOPPED FROM BUTTON");
+
+        }
+    }
+
+    private void downloadModelsFromFirebase() {
+        downloadNames();
+    }
+
+    private void downloadNames() {
+        StorageReference namesRef = this.mStorageRef.child("coco.names");
+
+        File localFile;
+        //localFile = File.createTempFile("coco_", ".names");
+        localFile = new File(getApplicationInfo().dataDir + File.separator + "coco.names");
+
+        File finalLocalFile = localFile;
+        namesRef.getFile(localFile)
+                .addOnSuccessListener(taskSnapshot -> {
+                    Log.d("NAMES_DOWNLOAD", "File downloaded");
+                    FileInputStream namesStream = null;
+                    try {
+                        namesStream = new FileInputStream(finalLocalFile);
+                    } catch (FileNotFoundException e) {
+                        e.printStackTrace();
+                    }
+                    assert namesStream != null;
+                    loadNamesOfClasses(namesStream);
+                    downloadConfig();
+                })
+                .addOnFailureListener(exception -> Log.d("NAMES_DOWNLOAD", "File not downloaded"));
+    }
+
+    private void downloadConfig() {
+        StorageReference namesRef = this.mStorageRef.child("yolov3.cfg");
+
+        File localFile;
+        //localFile = File.createTempFile("yolov3_", ".cfg");
+        localFile = new File(getApplicationInfo().dataDir + File.separator + "yolov3.cfg");
+
+        File finalLocalFile = localFile;
+        namesRef.getFile(localFile)
+                .addOnSuccessListener(taskSnapshot -> {
+                    Log.d("CONFIG_DOWNLOAD", "File downloaded");
+                    this.tinyYoloCfg = finalLocalFile.getPath();
+                    downloadWeights();
+                })
+                .addOnFailureListener(exception -> Log.d("CONFIG_DOWNLOAD", "File not downloaded"));
+    }
+
+    private void downloadWeights() {
+        StorageReference namesRef = this.mStorageRef.child("yolov3.weights");
+
+        File localFile;
+        //localFile = File.createTempFile("yolov3_", ".weights");
+        localFile = new File(getApplicationInfo().dataDir + File.separator + "yolov3.weights");
+
+        File finalLocalFile = localFile;
+        namesRef.getFile(localFile)
+                .addOnSuccessListener(taskSnapshot -> {
+                    Log.d("WEIGHTS_DOWNLOAD", "File downloaded");
+                    this.tinyYoloWeights = finalLocalFile.getPath();
+                    initializeNet();
+                })
+                .addOnFailureListener(exception -> Log.d("WEIGHTS_DOWNLOAD", "File not downloaded"));
+    }
+
+    private void initializeNet() {
+        tinyYolo = Dnn.readNetFromDarknet(tinyYoloCfg, tinyYoloWeights);
+        netInitialized = true;
+    }
+
+    private void loadNamesOfClasses(FileInputStream namesStream) {
+        BufferedReader reader;
+        this.cocoNames = new ArrayList<>();
+        try {
+            reader = new BufferedReader(new FileReader(namesStream.getFD()));
+            String line = reader.readLine();
+            while (line != null) {
+                this.cocoNames.add(line);
+                line = reader.readLine();
+            }
+            reader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static List<String> getOutputNames(Net net) {
+        List<String> names = new ArrayList<>();
+
+        List<Integer> outLayers = net.getUnconnectedOutLayers().toList();
+        List<String> layersNames = net.getLayerNames();
+
+        //unfold and create R-CNN layers from the loaded YOLO model//
+        for (Integer item: outLayers) {
+            names.add(layersNames.get(item - 1));
+        }
+        return names;
+    }
+
+
 }

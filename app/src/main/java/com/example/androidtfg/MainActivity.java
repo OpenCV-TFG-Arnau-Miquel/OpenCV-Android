3cefdcdbffc3f75d335bf37405e0af2563596f2c
ar98nau <anr3@alumnes.udl.cat>
2020-08-18 17:41:18 +0200

    Deleted button and added onStart functionality. Permissions control added
    Signed-off-by: ar98nau <anr3@alumnes.udl.cat>

--- app/src/main/java/com/example/androidtfg/MainActivity.java
+++ app/src/main/java/com/example/androidtfg/MainActivity.java
@@ -1,25 +1,21 @@
 package com.example.androidtfg;
 
-import android.Manifest;
-import android.app.Activity;
+import android.content.DialogInterface;
 import android.content.pm.PackageManager;
 import android.os.Build;
 import android.os.Bundle;
-
-import androidx.annotation.NonNull;
-import androidx.core.app.ActivityCompat;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.util.Log;
 import android.view.SurfaceView;
-import android.view.View;
 import android.widget.Toast;
 
-import com.example.androidseries.R;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 
-import org.opencv.android.BaseLoaderCallback;
 import org.opencv.android.CameraBridgeViewBase;
 import org.opencv.android.JavaCameraView;
 import org.opencv.android.OpenCVLoader;
@@ -44,21 +40,19 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
-
-
-
-
+import static android.Manifest.permission.CAMERA;
+import static android.Manifest.permission.READ_EXTERNAL_STORAGE;
+import static android.Manifest.permission.WRITE_EXTERNAL_STORAGE;
 
 
 public class MainActivity extends AppCompatActivity implements CameraBridgeViewBase.CvCameraViewListener2 {
 
     private static final String TAG = "MainActivity";
-    private static final int MY_CAMERA_REQUEST_CODE = 100;
     CameraBridgeViewBase cameraBridgeViewBase;
-    BaseLoaderCallback baseLoaderCallback;
-    boolean startYolo = false;
     Net tinyYolo;
     String tinyYoloCfg;
     String tinyYoloWeights;
@@ -66,46 +60,26 @@ public class MainActivity extends AppCompatActivity implements CameraBridgeViewB
     private ArrayList<String> cocoNames;
     private boolean netInitialized = false;
 
-    private static final int REQUEST_EXTERNAL_STORAGE = 1;
-    private static String[] PERMISSIONS_STORAGE = {
-            Manifest.permission.READ_EXTERNAL_STORAGE,
-            Manifest.permission.WRITE_EXTERNAL_STORAGE
-    };
+    //private ProgressBar progressBar;
 
+    public static final int REQUEST_ID_MULTIPLE_PERMISSIONS= 3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        cameraBridgeViewBase = (JavaCameraView)findViewById(R.id.CameraView);
+        cameraBridgeViewBase = (JavaCameraView) findViewById(R.id.CameraView);
         cameraBridgeViewBase.setVisibility(SurfaceView.VISIBLE);
         cameraBridgeViewBase.setCvCameraViewListener(this);
 
+        //progressBar = findViewById(R.id.progressBar);
+
         mStorageRef = FirebaseStorage.getInstance().getReference();
 
-        verifyStoragePermissions(this);
-
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            if (checkSelfPermission(Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
-                requestPermissions(new String[]{Manifest.permission.CAMERA}, MY_CAMERA_REQUEST_CODE);
-            }
+        if (!OpenCVLoader.initDebug()) {
+            Toast.makeText(getApplicationContext(), "There's a problem", Toast.LENGTH_SHORT).show();
         }
-
-        baseLoaderCallback = new BaseLoaderCallback(this) {
-            @Override
-            public void onManagerConnected(int status) {
-                super.onManagerConnected(status);
-
-                if (status == BaseLoaderCallback.SUCCESS) {
-                    cameraBridgeViewBase.enableView();
-                } else {
-                    super.onManagerConnected(status);
-                }
-
-            }
-
-        };
 
     }
 
@@ -114,6 +88,8 @@ public class MainActivity extends AppCompatActivity implements CameraBridgeViewB
         Log.d(TAG, "onStart");
 
         super.onStart();
+
+        requestAppPermissions();
     }
 
     @Override
@@ -121,16 +97,6 @@ public class MainActivity extends AppCompatActivity implements CameraBridgeViewB
         Log.d(TAG, "onResume");
 
         super.onResume();
-
-        if (!OpenCVLoader.initDebug()){
-            Toast.makeText(getApplicationContext(),"There's a problem, yo!", Toast.LENGTH_SHORT).show();
-        }
-
-        else
-        {
-            baseLoaderCallback.onManagerConnected(baseLoaderCallback.SUCCESS);
-        }
-
 
 
     }
@@ -140,7 +106,7 @@ public class MainActivity extends AppCompatActivity implements CameraBridgeViewB
         Log.d(TAG, "onPause");
 
         super.onPause();
-        if(cameraBridgeViewBase!=null){
+        if (cameraBridgeViewBase != null) {
 
             cameraBridgeViewBase.disableView();
         }
@@ -159,7 +125,7 @@ public class MainActivity extends AppCompatActivity implements CameraBridgeViewB
         Log.d(TAG, "onDestroy");
 
         super.onDestroy();
-        if (cameraBridgeViewBase!=null){
+        if (cameraBridgeViewBase != null) {
             cameraBridgeViewBase.disableView();
         }
 
@@ -170,11 +136,11 @@ public class MainActivity extends AppCompatActivity implements CameraBridgeViewB
 
         Mat frame = inputFrame.rgba();
 
-        if (startYolo && netInitialized) {
+        if (netInitialized) {
 
             Imgproc.cvtColor(frame, frame, Imgproc.COLOR_RGBA2RGB);
 
-            Mat imageBlob = Dnn.blobFromImage(frame, 0.00392, new Size(416,416),new Scalar(0, 0, 0),/*swapRB*/false, /*crop*/false);
+            Mat imageBlob = Dnn.blobFromImage(frame, 0.00392, new Size(416, 416), new Scalar(0, 0, 0),/*swapRB*/false, /*crop*/false);
 
             tinyYolo.setInput(imageBlob);
 
@@ -182,7 +148,7 @@ public class MainActivity extends AppCompatActivity implements CameraBridgeViewB
 
             List<String> outBlobNames = getOutputNames(tinyYolo);
 
-            tinyYolo.forward(result,outBlobNames);
+            tinyYolo.forward(result, outBlobNames);
 
             float confThreshold = 0.3f;
 
@@ -190,33 +156,30 @@ public class MainActivity extends AppCompatActivity implements CameraBridgeViewB
             List<Float> confs = new ArrayList<>();
             List<Rect> rects = new ArrayList<>();
 
-            for (int i = 0; i < result.size(); ++i)
-            {
+            for (int i = 0; i < result.size(); ++i) {
 
                 Mat level = result.get(i);
 
-                for (int j = 0; j < level.rows(); ++j)
-                {
+                for (int j = 0; j < level.rows(); ++j) {
                     Mat row = level.row(j);
                     Mat scores = row.colRange(5, level.cols());
 
                     Core.MinMaxLocResult mm = Core.minMaxLoc(scores);
 
-                    float confidence = (float)mm.maxVal;
+                    float confidence = (float) mm.maxVal;
 
                     Point classIdPoint = mm.maxLoc;
 
-                    if (confidence > confThreshold)
-                    {
-                        int centerX = (int)(row.get(0,0)[0] * frame.cols());
-                        int centerY = (int)(row.get(0,1)[0] * frame.rows());
-                        int width   = (int)(row.get(0,2)[0] * frame.cols());
-                        int height  = (int)(row.get(0,3)[0] * frame.rows());
+                    if (confidence > confThreshold) {
+                        int centerX = (int) (row.get(0, 0)[0] * frame.cols());
+                        int centerY = (int) (row.get(0, 1)[0] * frame.rows());
+                        int width = (int) (row.get(0, 2)[0] * frame.cols());
+                        int height = (int) (row.get(0, 3)[0] * frame.rows());
 
-                        int left    = centerX - width  / 2;
-                        int top     = centerY - height / 2;
+                        int left = centerX - width / 2;
+                        int top = centerY - height / 2;
 
-                        clsIds.add((int)classIdPoint.x);
+                        clsIds.add((int) classIdPoint.x);
                         confs.add(confidence);
 
                         rects.add(new Rect(left, top, width, height));
@@ -225,7 +188,7 @@ public class MainActivity extends AppCompatActivity implements CameraBridgeViewB
             }
             int ArrayLength = confs.size();
 
-            if (ArrayLength>=1) {
+            if (ArrayLength >= 1) {
                 // Apply non-maximum suppression procedure.
                 float nmsThresh = 0.2f;
 
@@ -253,14 +216,13 @@ public class MainActivity extends AppCompatActivity implements CameraBridgeViewB
 
                     int intConf = (int) (conf * 100);
 
-                    Imgproc.putText(frame,cocoNames.get(idGuy) + " " + intConf + "%",box.tl(),Core.FONT_HERSHEY_SIMPLEX, 2, new Scalar(255,255,0),2);
+                    Imgproc.putText(frame, cocoNames.get(idGuy) + " " + intConf + "%", box.tl(), Core.FONT_HERSHEY_SIMPLEX, 2, new Scalar(255, 255, 0), 2);
 
                     Imgproc.rectangle(frame, box.tl(), box.br(), new Scalar(255, 0, 0), 2);
 
                 }
             }
         }
-
 
 
         return frame;
@@ -281,49 +243,112 @@ public class MainActivity extends AppCompatActivity implements CameraBridgeViewB
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        if (requestCode == MY_CAMERA_REQUEST_CODE) {
-            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                Toast.makeText(this, "camera permission granted", Toast.LENGTH_LONG).show();
-            } else {
-                Toast.makeText(this, "camera permission denied", Toast.LENGTH_LONG).show();
+        switch (requestCode) {
+            case REQUEST_ID_MULTIPLE_PERMISSIONS: {
+
+                Map<String, Integer> perms = new HashMap<>();
+                perms.put(CAMERA, PackageManager.PERMISSION_GRANTED);
+                perms.put(WRITE_EXTERNAL_STORAGE, PackageManager.PERMISSION_GRANTED);
+                perms.put(READ_EXTERNAL_STORAGE, PackageManager.PERMISSION_GRANTED);
+                if (grantResults.length > 0) {
+                    for (int i = 0; i < permissions.length; i++) {
+                        perms.put(permissions[i], grantResults[i]);
+                    }
+
+                    if (perms.get(WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED
+                            && perms.get(CAMERA) == PackageManager.PERMISSION_GRANTED
+                            && perms.get(READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
+                        cameraBridgeViewBase.enableView();
+                        YOLO();
+                    } else {
+                        if (ActivityCompat.shouldShowRequestPermissionRationale(this, WRITE_EXTERNAL_STORAGE) || ActivityCompat.shouldShowRequestPermissionRationale(this, CAMERA) || ActivityCompat.shouldShowRequestPermissionRationale(this, READ_EXTERNAL_STORAGE)) {
+                            showDialogOK("You must accept Camera and Storage Permission as they are required for this app",
+                                    (dialog, which) -> {
+                                        switch (which) {
+                                            case DialogInterface.BUTTON_POSITIVE:
+                                                requestAppPermissions();
+                                                break;
+                                            case DialogInterface.BUTTON_NEGATIVE:
+                                                finish();
+                                                break;
+                                        }
+                                    });
+                        } else {
+                            Toast.makeText(this, "You should enable permissions in your device settings", Toast.LENGTH_LONG).show();
+                        }
+                    }
+                }
             }
         }
     }
 
-    public static void verifyStoragePermissions(Activity activity) {
-        // Check if we have write permission
-        int permission = ActivityCompat.checkSelfPermission(activity, Manifest.permission.WRITE_EXTERNAL_STORAGE);
-
-        if (permission != PackageManager.PERMISSION_GRANTED) {
-            // We don't have permission so prompt the user
-            ActivityCompat.requestPermissions(
-                    activity,
-                    PERMISSIONS_STORAGE,
-                    REQUEST_EXTERNAL_STORAGE
-            );
+    private void requestAppPermissions() {
+        List<String> permissions =  new ArrayList<>();
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (checkSelfPermission(CAMERA) != PackageManager.PERMISSION_GRANTED) {
+                permissions.add(CAMERA);
+            }
+            if (checkSelfPermission(WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+                permissions.add(WRITE_EXTERNAL_STORAGE);
+            }
+            if (checkSelfPermission(READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+                permissions.add(READ_EXTERNAL_STORAGE);
+            }
+            if (!permissions.isEmpty()) {
+                requestPermissions(permissions.toArray(new String[0]), REQUEST_ID_MULTIPLE_PERMISSIONS);
+            }
+        }
+        if (permissions.isEmpty()) {
+            cameraBridgeViewBase.enableView();
+            YOLO();
         }
     }
 
-    public void YOLO(View Button){
+    private void showDialogOK(String message, DialogInterface.OnClickListener okListener) {
+        new AlertDialog.Builder(this)
+                .setMessage(message)
+                .setPositiveButton("OK", okListener)
+                .setNegativeButton("Cancel", okListener)
+                .create()
+                .show();
+    }
 
-        if (!startYolo){
+    public void YOLO() {
 
-            startYolo = true;
+        //this.progressBar.setVisibility(View.VISIBLE);
+        if (!netInitialized) {
 
-            if (!netInitialized){
+            String path = getApplicationInfo().dataDir;
+            File directory = new File(path);
+            File[] files = directory.listFiles((dir, name) -> name.contains("."));
+
+            if (files.length != 3) {
                 downloadModelsFromFirebase();
-                Log.d(TAG, "STARTED FROM BUTTON - FIRST TIME");
 
+            } else {
+                for (File file : files) {
+                    if ("coco.names".equals(file.getName())) {
+                        try {
+                            loadNamesOfClasses(new FileInputStream(file));
+                        } catch (FileNotFoundException e) {
+                            e.printStackTrace();
+                        }
+                    } else if ("yolov3.cfg".equals(file.getName())) {
+                        this.tinyYoloCfg = file.getPath();
+                    } else {
+                        this.tinyYoloWeights = file.getPath();
+                    }
+                }
+                if (tinyYoloCfg != null && tinyYoloWeights != null && cocoNames != null && !cocoNames.isEmpty()) {
+                    initializeNet();
+
+                } else {
+                    finish();
+                }
             }
-
-            Log.d(TAG, "STARTED FROM BUTTON");
-
-        } else {
-
-            startYolo = false;
-            Log.d(TAG, "STOPPED FROM BUTTON");
-
+            Log.d(TAG, "STARTED FROM CREATE - FIRST TIME");
         }
+        //this.progressBar.setVisibility(View.GONE);
     }
 
     private void downloadModelsFromFirebase() {
@@ -391,6 +416,7 @@ public class MainActivity extends AppCompatActivity implements CameraBridgeViewB
     private void initializeNet() {
         tinyYolo = Dnn.readNetFromDarknet(tinyYoloCfg, tinyYoloWeights);
         netInitialized = true;
+        //this.progressBar.setVisibility(View.GONE);
     }
 
     private void loadNamesOfClasses(FileInputStream namesStream) {
@@ -416,7 +442,7 @@ public class MainActivity extends AppCompatActivity implements CameraBridgeViewB
         List<String> layersNames = net.getLayerNames();
 
         //unfold and create R-CNN layers from the loaded YOLO model//
-        for (Integer item: outLayers) {
+        for (Integer item : outLayers) {
             names.add(layersNames.get(item - 1));
         }
         return names;
